07010c3551ae5b4c795ec1df59beb154f56e2d49
Sail3shp <saileshpokhrel557@gmail.com>
2025-11-07 20:10:39 +0545

    deployment practise

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,36 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import rateLimiter from './middleware/rateLimiter.js'
+import path from 'path'
 
 import { connectDB } from './config/db.js'
 import notesRoutes from './routes/notesRoutes.js'
 dotenv.config()
 
 const app = express()
-const PORT =  process.env.PORT || 9999
+const PORT = process.env.PORT || 9999
+const __dirname = path.resolve()
 
-app.use(cors())
+if(process.env.NODE_ENV !== 'production'){
+app.use(cors({
+    origin:"http://localhost:5173"
+}
+))
+}
 app.use(express.json())
 app.use(rateLimiter)
-app.use('/api/notes',notesRoutes)
+app.use('/api/notes', notesRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 connectDB().then(() => {
-    app.listen(PORT,() => {
-    console.log('server is running on port:',PORT)
-})
+    app.listen(PORT, () => {
+        console.log('server is running on port:', PORT)
+    })
 })
--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
+
+const BASE_URL = import.meta.env.MODE === "development" ? 'http://localhost:8888/api/notes' : "/api/notes"
 const api = axios.create({
-    baseURL: 'http://localhost:8888/api/notes'
+    baseURL: BASE_URL,
 })
 export default api
