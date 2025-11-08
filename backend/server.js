58c20902111f30b99cd4b8a2cf8b0163e343c664
Sail3shp <saileshpokhrel557@gmail.com>
2025-11-08 09:58:45 +0545

    w

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,8 @@ app.use('/api/notes', notesRoutes)
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get('/:path*', (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    app.use((req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
     })
 }
 
